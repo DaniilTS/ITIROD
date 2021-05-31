3db3c227576cfd22558cfc765aa7411cf53ef561
DaniilTS <d.tsukrov@mail.ru>
2021-06-01 00:00:02 +0300

    add: createNew page db inserting

--- public/js/createNew.js
+++ public/js/createNew.js
@@ -19,8 +19,45 @@ addReminderBtn.addEventListener('click', (e)=>{
 });
 
 createNewBtn.addEventListener('click', (e)=>{
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const start = document.getElementById('start').value;
+    const end = document.getElementById('end').value;
+    const place = document.getElementById('place').value;
+    const color = document.getElementById('color').value;
 
+    const checkResult = checkRemindersInputs();
+
+    if(title && description && start && end && color && checkResult[0]){
+        const taskOrAppointment = leftRadioBtn.checked ? 'tasks' : 'appointments';
+        database.ref(`users/${auth.currentUser.uid}/${taskOrAppointment}`).set({
+            title: title,
+            description: description,
+            start: start,
+            end: end,
+            place: place,
+            color: color,
+            reminders: checkResult[1]
+        });
+    } else {
+        alert('fill all inputs and reminders');
+    }
 });
+
+function checkRemindersInputs(){
+    const liList = remindersList.getElementsByTagName('li');
+    const listLength = liList.length;
+    let remindersArray = [];
+
+    for(let i = 0; i < listLength; i++){
+        let reminder = liList[i].children[0].value;
+        if(!reminder) {
+            return [false, null];
+        }
+        remindersArray.push(reminder);
+    }
+    return [true, remindersArray];
+}
 
 function createDateTimeInput(){
     let dateTimeInput = document.createElement('input');
@@ -40,11 +77,7 @@ function createDeleteReminderBtn(reminder){
     btn.type = 'button';
     btn.innerText = 'Delete';
     btn.addEventListener('click', (e)=>{
-        if(remindersList.getElementsByTagName('li').length > 1) {
-            remindersList.removeChild(reminder);
-        } else {
-            alert("you can't delete last remainder.");
-        }
+        remindersList.removeChild(reminder);
     });
     return btn;
 }
