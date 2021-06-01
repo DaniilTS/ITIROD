6e567a0a191190394ba661042a2a13a3584bfda6
DaniilTS <d.tsukrov@mail.ru>
2021-06-01 23:22:41 +0300

    done: 'createNew' page

--- public/js/createNew.js
+++ public/js/createNew.js
@@ -4,21 +4,19 @@ const addReminderBtn = document.getElementById('addReminderBtn');
 const remindersList = document.getElementById('remindersList');
 const createNewBtn = document.getElementById('createNewBtn');
 
-createReminder();
-
-leftRadioBtn.addEventListener('click', (e)=>{
-   rightRadioBtn.checked = false;
+leftRadioBtn.addEventListener('click', (e) => {
+    rightRadioBtn.checked = false;
 });
 
-rightRadioBtn.addEventListener('click', (e)=>{
+rightRadioBtn.addEventListener('click', (e) => {
     leftRadioBtn.checked = false;
 });
 
-addReminderBtn.addEventListener('click', (e)=>{
+addReminderBtn.addEventListener('click', (e) => {
     createReminder();
 });
 
-createNewBtn.addEventListener('click', (e)=>{
+createNewBtn.addEventListener('click', (e) => {
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
     const start = document.getElementById('start').value;
@@ -26,63 +24,82 @@ createNewBtn.addEventListener('click', (e)=>{
     const place = document.getElementById('place').value;
     const color = document.getElementById('color').value;
 
-    const checkResult = checkRemindersInputs();
-
-    if(title && description && start && end && color && checkResult[0]){
+    const remindersCheckResult = checkRemindersInputs();
+    if (title && description && start && end && color && remindersCheckResult.allInputsFilled) {
         const taskOrAppointment = leftRadioBtn.checked ? 'tasks' : 'appointments';
-        database.ref(`users/${auth.currentUser.uid}/${taskOrAppointment}`).set({
-            title: title,
-            description: description,
-            start: start,
-            end: end,
-            place: place,
-            color: color,
-            reminders: checkResult[1]
+
+        database.ref(`users/${auth.currentUser.uid}/${taskOrAppointment}`)
+            .get().then((snapshot) => {
+
+            const amountOfElements = snapshot.val() ? snapshot.val().length : 0;
+            const taskOrAppointmentDbRef = database.ref(`users/${auth.currentUser.uid}/${taskOrAppointment}/${amountOfElements}`);
+
+            taskOrAppointmentDbRef.set({
+                title: title,
+                description: description,
+                start: dateTimeLocalToSeconds(start),
+                end: dateTimeLocalToSeconds(end),
+                place: place,
+                color: color,
+                reminders: remindersCheckResult.reminders
+            });
+
+            if (taskOrAppointment === 'tasks') {
+                taskOrAppointmentDbRef.update({
+                    isDone: false
+                });
+            }
+
+            setTimeout(RedirectTo('Main'), 1000);
         });
     } else {
         alert('fill all inputs and reminders');
     }
 });
 
-function checkRemindersInputs(){
+function checkRemindersInputs() {
     const liList = remindersList.getElementsByTagName('li');
     const listLength = liList.length;
     let remindersArray = [];
 
-    for(let i = 0; i < listLength; i++){
-        let reminder = liList[i].children[0].value;
-        if(!reminder) {
-            return [false, null];
+    for (let i = 0; i < listLength; i++) {
+        let reminderTime = liList[i].children[0].value;
+        if (!reminderTime) {
+            return {
+                allInputsFilled: false
+            };
         }
-        remindersArray.push(reminder);
+        remindersArray.push(dateTimeLocalToSeconds(reminderTime));
     }
-    return [true, remindersArray];
+
+    return {
+        allInputsFilled: true,
+        reminders: remindersArray
+    };
 }
 
-function createDateTimeInput(){
+function createDateTimeInput() {
     let dateTimeInput = document.createElement('input');
     dateTimeInput.setAttribute('class', 'creation-form__input reminder-input');
     dateTimeInput.type = 'datetime-local';
     dateTimeInput.value = Date();
-    dateTimeInput.addEventListener('change', (e)=>{
-
-    });
 
     return dateTimeInput;
 }
 
-function createDeleteReminderBtn(reminder){
+function createDeleteReminderBtn(reminder) {
     const btn = document.createElement('button');
     btn.setAttribute('class', 'creation-form__button delete-button');
     btn.type = 'button';
     btn.innerText = 'Delete';
-    btn.addEventListener('click', (e)=>{
+    btn.addEventListener('click', (e) => {
         remindersList.removeChild(reminder);
     });
+
     return btn;
 }
 
-function createReminder(){
+function createReminder() {
     const reminder = document.createElement('li');
     reminder.setAttribute('class', 'reminders-list__item');
 
@@ -93,4 +110,8 @@ function createReminder(){
     reminder.appendChild(deleteReminderBtn);
 
     remindersList.appendChild(reminder);
+}
+
+function dateTimeLocalToSeconds(dateTimeValue) {
+    return Math.round(new Date(dateTimeValue) / 1000).toString();
 }
